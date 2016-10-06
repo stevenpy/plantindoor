fe5d7e9ffa38c0090ef42d9a3f4e420436753c07
stevenpy <stevenphilbert@gmail.com>
2016-10-06 15:37:41 -0400

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :members
+  
   get 'main/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  resources :members
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
