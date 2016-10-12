0e66166baaafdcb91157f81ceade9e6ee95186ec
stevenpy <stevenphilbert@gmail.com>
2016-10-11 21:08:09 -0400

    Remove mailchimp

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-	after_save :add_member_to_mailchimp
+	# after_save :add_member_to_mailchimp
 	validates :email, presence: true
 
 	private
@@ -7,5 +7,7 @@ class Member < ActiveRecord::Base
 	def add_member_to_mailchimp
 		gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"])
 		gibbon.lists("18edd4b1cd").members.create(body: {email_address: email, status: "subscribed"})
+		# rescue Gibbon::MailChimpError => e
+			# self.errors.add(:email, e.title) if e.body['status'] == 400 && e.title == 'Member Exists'
 	end
 end
