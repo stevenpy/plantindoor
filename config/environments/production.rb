38338e93d1fd384de986c18c199bd7d328a57163
stevenpy <stevenphilbert@gmail.com>
2016-10-17 23:35:31 -0400

    Edit production env to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
 
+  config.action_mailer.delivery_method = :sendmail
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtpout.secureserver.net',
