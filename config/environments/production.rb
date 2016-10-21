faeee19d155f26fcc745202c9c16e714fd77407d
stevenpy <stevenphilbert@gmail.com>
2016-10-20 21:10:59 -0400

    Test removing sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'contact@plantindoor.com'}
 
-  config.action_mailer.delivery_method = :sendmail
+  
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
