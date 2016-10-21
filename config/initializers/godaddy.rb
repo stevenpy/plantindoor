17ec55bf97e23b3cb8f8c5b1ba3dffc7af9db461
stevenpy <stevenphilbert@gmail.com>
2016-10-20 23:09:31 -0400

    Test with gmail

--- config/initializers/godaddy.rb
+++ config/initializers/godaddy.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   user_name:            ENV['GODADDY_USERNAME'],
   password:             ENV['GODADDY_PSW'],
   domain:               'plantindoor.com',
-  address:              'smtpout.secureserver.net',
-  port:                 80,
+  address:              'smtp.gmail.com',
+  port:                 587,
   authentication:       'plain',
   enable_starttls_auto: true,
 }
