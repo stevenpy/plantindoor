0ba5a2e94f38fe4fae240b7fa799f71c9e01a842
stevenpy <stevenphilbert@gmail.com>
2016-10-20 21:07:02 -0400

    Test with simple mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'contact@plantindoor.com'}
-  config.action_mailer.default_url_options = {host: 'plantindoor.herokuapp.com'}
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
